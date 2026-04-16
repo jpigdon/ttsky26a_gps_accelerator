5bdee0297ee38329ba3a2725fb1a84a8768c4da7
jpigdon <joel@vtxtechnologies.com>
2026-04-17 02:24:53 +1000

    Tested rollover with gold code and added and tested nco

--- test/test_gold_code.py
+++ test/test_gold_code.py
@@ -24,6 +24,8 @@ async def test_gold_code(dut):
     for i in range(ca_code_gen.num_sv()):
         
         prn_seq = ca_code_gen.PRN(i+1)
+        prn_seq.extend(prn_seq)
+        prn_seq.extend(prn_seq)
 
         dut.sv_taps.value = ca_code_gen.taps_from_sv(i+1)
         print("PRN IDX: " + str(i) + " Taps: " + str(hex(ca_code_gen.taps_from_sv(i+1))))
@@ -33,8 +35,8 @@ async def test_gold_code(dut):
         dut.sv_load.value = 0
 
 
-        # wait pwm level clock steps
-        for chip_idx in range(1023):
+        #check the wraparound works correctly
+        for chip_idx in range(1023*4):
             await RisingEdge(dut.clk)
 
             if ASSERT:
